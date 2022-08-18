213b72079a1e40e0981b522e98ccd390c632ce88
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-18 19:33:43 -0400

    fixed bolt path

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,14 @@ import megaDougBoltLogo from "./images/megadoug_bolt_in_circle_cutout_grey.svg";
 const Footer = () => {
     return (
         <footer>
-            ©2022 MegaloMedia,<img
-                        className="megaDoug-bolt-img"
-                        src={megaDougBoltLogo}
-                        alt="megaDoug bolt Logo"
-                    />megaDoug
+            ©2022 MegaloMedia,
+            <img
+                style={{ height: "16px", width: auto }}
+                className="megaDoug-bolt-img"
+                src={megaDougBoltLogo}
+                alt="megaDoug bolt Logo"
+            />
+            megaDoug
         </footer>
     );
 };
