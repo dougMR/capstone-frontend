484bd1f4ae366a74c7ea748498154407ba58a664
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-18 20:33:41 -0400

    fixed APIUrl to work w heroku

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,2 +1,6 @@
-const APIUrl = "http://localhost:3001";
+let APIUrl = "http://localhost:3001";
+// check if we're running on heroku
+if(window.location.host.indexOf('.herokuapp.com') !== -1){
+    APIUrl = "https://dougmr-capstone-backend.herokuapp.com";
+}
 export default APIUrl;
